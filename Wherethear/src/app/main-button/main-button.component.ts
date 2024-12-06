0bb2aef1eff9664c99532a29585df248bbf5a1d3
Dan Kleczewski <Dan.Kleczewski1@etu.univ-paris1.fr>
2024-12-06 03:04:24 +0100

    button changing

--- Wherethear/src/app/main-button/main-button.component.ts
+++ Wherethear/src/app/main-button/main-button.component.ts
@@ -1,16 +1,16 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-main-button',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './main-button.component.html',
   styleUrl: './main-button.component.css'
 })
 export class MainButtonComponent {
   changePicture() {
-    console.log('aa');
-    if (this.pictureRef = 'https://cdn-icons-png.flaticon.com/512/5213/5213449.png'){
+    if (this.pictureRef === 'https://cdn-icons-png.flaticon.com/512/5213/5213449.png'){
       this.pictureRef = 'https://cdn-icons-png.flaticon.com/512/10480/10480648.png';
     } else {
       this.pictureRef = 'https://cdn-icons-png.flaticon.com/512/5213/5213449.png';
