29ab678fd02b7e36e104944097e34bcfcd88e106
Dan Kleczewski <Dan.Kleczewski1@etu.univ-paris1.fr>
2024-12-06 02:41:51 +0100

    mainbutton

--- Wherethear/src/app/body-front-page/body-front-page.component.ts
+++ Wherethear/src/app/body-front-page/body-front-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MainButtonComponent } from "../main-button/main-button.component";
 
 @Component({
   selector: 'app-body-front-page',
   standalone: true,
-  imports: [],
+  imports: [MainButtonComponent],
   templateUrl: './body-front-page.component.html',
   styleUrl: './body-front-page.component.css'
 })
--- Wherethear/src/app/main-button/main-button.component.ts
+++ Wherethear/src/app/main-button/main-button.component.ts
@@ -8,5 +8,15 @@ import { Component } from '@angular/core';
   styleUrl: './main-button.component.css'
 })
 export class MainButtonComponent {
+  changePicture() {
+    console.log('aa');
+    if (this.pictureRef = 'https://cdn-icons-png.flaticon.com/512/5213/5213449.png'){
+      this.pictureRef = 'https://cdn-icons-png.flaticon.com/512/10480/10480648.png';
+    } else {
+      this.pictureRef = 'https://cdn-icons-png.flaticon.com/512/5213/5213449.png';
+    }
+  }
+
+pictureRef: String = 'https://cdn-icons-png.flaticon.com/512/5213/5213449.png'
 
 }
